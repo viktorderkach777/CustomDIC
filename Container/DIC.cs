f637224580ce83b749d2ab8b3b80dfc9eaf6656c
viktorderkach777 <viktorderkach777@yandex.ua>
2019-03-04 20:14:16 +0200

    create DIC

---
+++ Container/DIC.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container
+{
+    public class InvalidAbstractionType:Exception
+    {
+        public override string Message => "Parameter is either Interface or Abstract class";
+    }
+
+
+    public class DIC
+    {
+        public IDictionary<Type, Type> TypesStorage { get; private set; }
+
+        public DIC()
+        {
+            TypesStorage = new Dictionary<Type, Type>();
+        }
+
+        public void Register <T1, T2>()
+        {
+            var type1 = typeof(T1);
+            var type2 = typeof(T2);
+
+            if (type2.IsAbstract || type2.IsInterface)
+            {
+                throw new InvalidAbstractionType();
+            }
+
+            if (!type1.IsAbstract && !type1.IsInterface)
+            {
+                throw new InvalidAbstractionType();
+            }
+
+            TypesStorage.Add(type1, type2);
+        }
+
+        public T1 Resolve<T1>()
+        {
+            var myType = TypesStorage[typeof(T1)];
+
+            var instance = Activator.CreateInstance(myType);
+            return (T1)instance;
+        }
+    }
+}
--- MainCode/Program.cs
+++ MainCode/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Container;
 
 namespace MainCode
 {
@@ -10,6 +11,44 @@ namespace MainCode
     {
         static void Main(string[] args)
         {
+            DIC dc = new DIC();
+
+            dc.Register<ISomeInterface, UsefulClass >(); //A1, R1:A1
+            dc.Register<MyAbstractClass, SomeClassRealization>();//A2, R2:A2
+
+            //Console.WriteLine(dc.Resolve<ISomeInterface>().IsDoingNothingMethod());
+            //Console.WriteLine(dc.Resolve<MyAbstractClass>().UselessMethod());
+
+
+            Console.WriteLine(dc.Resolve<ISomeInterface>().IsDoingNothingMethod());//A1, R2:A2
+            Console.WriteLine(dc.Resolve<MyAbstractClass>().UselessMethod());//R1:A1, A2
         }
+    }
+
+    public interface ISomeInterface
+    {
+        bool IsDoingNothingMethod();
+    }
+
+    public class SomeClassRealization : ISomeInterface
+    {
+        public bool IsDoingNothingMethod()
+        {
+            return true;
+        }
+    }
+
+    public abstract class MyAbstractClass
+    {
+        public abstract bool UselessMethod();
+    }
+
+
+    public class UsefulClass : MyAbstractClass
+    {
+        public override bool UselessMethod()
+        {
+            return false;
+        }       
     }
 }
